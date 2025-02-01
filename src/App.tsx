8d69b63047a08790956dc3a1e0aca9e9b37aae9d
Guru <gurugsv777@gmail.com>
2025-02-02 00:44:52 +0530

    Fixed GitHub Pages deployment issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Navigation } from './components/Navigation';
 import { Home } from './pages/Home';
 import { SignIn } from './pages/SignIn';
@@ -10,16 +10,12 @@ import { Resources } from './pages/Resources';
 
 function App() {
   return (
-    <Router basename="/elevatree">
+    <Router>
       <div className="min-h-screen flex flex-col">
         <Navigation />
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login" element={<SignIn />} />
-            <Route path="/mentors" element={<Mentors />} />
-            <Route path="/about" element={<AboutUs />} />
-            <Route path="/resources" element={<Resources />} />
             {/* Other routes will be added as we build them */}
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
@@ -30,5 +26,4 @@ function App() {
   );
 }
 
-// Change the export syntax
 export default App;
